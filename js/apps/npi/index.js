dc5c41116a2c0638213e5c91c34f066b1ebba25a
Ivanq <imachug@yandex.ru>
2018-04-16 22:33:00 +0300

    Add subcommand support

--- js/apps/npi/index.js
+++ js/apps/npi/index.js
@@ -6,12 +6,20 @@
 function main(args, api, res) {
 	const io = api.stdio;
 
-	if(args == "" || args == "help") {
+	args = args.split(/\s+/);
+	let cmd = args.shift();
+
+	if(cmd == "" || cmd == "help") {
 		io.setColor("yellow");
 		io.writeLine("JsOS/NPI - No Problem Installer");
 		io.writeLine("Commands:");
 		io.writeLine("help      Show command or subcommand help");
+		io.writeLine("info      Show package info");
 		return res(0);
+	} else if(cmd == "info") {
+		io.setColor("magenta");
+		io.writeLine("npi info not implemented");
+		return res(1);
 	} else {
 		io.setColor("red");
 		io.writeLine("Unknown command");
