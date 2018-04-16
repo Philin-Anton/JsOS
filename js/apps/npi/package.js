15674a0a9064a45d2b76d5371d88700f870e43eb
Ivanq <imachug@yandex.ru>
2018-04-16 22:50:17 +0300

    Add get()

--- js/apps/npi/package.js
+++ js/apps/npi/package.js
@@ -1,6 +1,33 @@
+const http = require("http");
+
 class Package {
 	constructor(name) {
 		this.name = name;
+	}
+
+	get(url, cb) {
+		return http.get(url, res => {
+			let str = "";
+			res.setEncoding("utf8");
+			res.on("data", chunk => {
+				str += chunk;
+			});
+			res.on("end", () => {
+				if(cb) {
+					cb(str);
+				}
+			});
+		});
+	}
+
+	corsGet(url, cb) {
+		return this.get("http://cors-anywhere.herokuapp.com/" + url, cb);
+	}
+
+	getInfo() {
+		this.corsGet("", str => {
+			console.log(str);
+		});
 	}
 };
 
